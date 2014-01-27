3bb9bbf4a7e2107ca3f2a10c8fe5e3da5f35a2a1
Ryan Moore <ryan.moore@greyrocksoft.com>
2014-01-27 12:45:37 -0700

    Creating a setup file so that the python package can be installed on other systems.

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='DS3 SDK',
+    version='1.0',
+    description='Python SDK and CLI for DS3',
+    author='Ryan Moore',
+    author_email='ryanmo@spectralogic.com',
+    packages=['ds3'])
