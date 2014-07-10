7c62cb38a709a49689ae732f6500093a5a8a0892
timp <timp@spectralogic.com>
2014-07-10 14:14:19 -0600

    add clear all objects from bucket test program ds3_clear_bucket.  Remove more verboseness from HTTP code

--- ds3/ds3.py
+++ ds3/ds3.py
@@ -700,26 +700,6 @@ class NetworkClient(object):
 
     def get_date(self):
         return formatdate()
-    
-class VerboseHTTPResponse(httplib.HTTPResponse):
-    def _read_status(self):
-        s = self.fp.read()
-        print '-' * 20, 'Response', '-' * 20
-        print s.split('\r\n\r\n')[0]
-        self.fp = StringIO.StringIO(s)
-        return httplib.HTTPResponse._read_status(self)
-    
-class VerboseHTTPConnection(httplib.HTTPConnection):
-    response_class = VerboseHTTPResponse
-    def send(self, s):
-        print '-' * 50
-        print s.strip()
-        httplib.HTTPConnection.send(self, s)
-
-class VerboseHTTPHandler(urllib2.HTTPHandler):
-    def http_open(self, req):
-        return self.do_open(VerboseHTTPConnection, req)
-
 
 class NetworkConnection(object):
     """
