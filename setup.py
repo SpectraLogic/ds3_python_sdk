05f7c435c08857d0eb5cb3e6985adcfbf925ec4e
timp <timp@spectralogic.com>
2014-07-11 12:42:26 -0600

    remove scripts directory in setup. Add 2.0 changlog description

--- setup.py
+++ setup.py
@@ -6,6 +6,5 @@ setup(name='DS3 SDK',
     version='2.0',
     description='Python SDK and CLI for DS3',
     author='Ryan Moore, Tim Powell',
-    author_email='ryanmo@spectralogic.com',
+    author_email='timp@spectralogic.com',
     packages=['ds3'],
-    scripts=['scripts/ds3_cli'])
