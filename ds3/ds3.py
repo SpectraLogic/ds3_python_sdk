8b3c7bf82cd7bb8d7c0de010dc6cff7af680ba46
Ryan Moore <ryan.moore@greyrocksoft.com>
2014-12-10 11:28:07 -0700

    Get Service is now fully wrapped

--- ds3/ds3.py
+++ ds3/ds3.py
@@ -1,10 +1,19 @@
 from ctypes import *
 import libds3
 
+class Ds3Error(Exception):
+    def __init__(self, libds3Error):
+        self.message = libds3Error.contents.message.value
+
 class Credentials(object):
     def __init__(self, accessKey, secretKey):
         self.accessKey = accessKey
         self.secretKey = secretKey
+
+class Ds3Bucket(object):
+    def __init__(self, ds3Bucket):
+        self.name = ds3Bucket.name.contents.value
+        self.creationDate = ds3Bucket.creation_date.contents.value
 
 class Ds3Client(object):
     def __init__(self, endpoint, credentials):
@@ -16,5 +25,9 @@ class Ds3Client(object):
         response = POINTER(libds3.LibDs3GetServiceResponse)()
         request = libds3.lib.ds3_init_get_service()
         error = libds3.lib.ds3_get_service(self._client, request, byref(response))
-        return response.contents
+        if error:
+            raise Ds3Error(error)
+        contents = response.contents
 
+        for i in xrange(0, response.contents.num_buckets):
+            yield Ds3Bucket(contents.buckets[i])
