841833fb946f1142505d3d20e47efe51c6ff6b30
Charles Hanchett <charlesh@spectralogic.com>
2015-09-25 15:44:18 -0600

    updated docs

--- sphinx/v1.0-0/source/conf.py
+++ sphinx/v1.0-0/source/conf.py
@@ -110,7 +110,8 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'classic'
+# In sphinx 1.3.1, the theme we want is called classic, and this line eventually needs to uncommented
+#html_theme = 'classic'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
