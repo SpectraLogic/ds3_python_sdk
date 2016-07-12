fcfdfed6c5de74621c0f07c86100808e0eaa34a5
Denver <denverm@spectralogic.com>
2016-07-12 15:18:51 -0600

    Update run_docker.rb

--- docker/run_docker.rb
+++ docker/run_docker.rb
@@ -1,18 +1,16 @@
 require "faraday"
 require "json"
 
-# Has to be to https and not http.  Insert your blackpearl box.
-connection = Faraday.new(:url => "https://sm25-2-mgmt.eng.sldomain.com",
-                         # Don't worry about verifying the SSL certificate.
-                         # In a production system, you'd want to verify it but
-                         # it doesn't matter for the cucumber tests.
-                         :ssl => { :verify => false }) do |conn|
+ENV["DS3_ENDPOINT"] = ENV["DS3_ENDPOINT"] || "sm2u-11"
+
+# Has to be to https and not http.
+connection = Faraday.new(:url =>  "https://#{ENV["DS3_ENDPOINT"]}-mgmt.eng.sldomain.com",
+                                            # Don't worry about verifying the SSL certificate.
+                                            :ssl => { :verify => false }) do |conn|
   conn.request(:basic_auth, "spectra", "spectra")
   # User Ruby's net/http library
   conn.adapter(:net_http)
-  # Setup for JSON requests and responses.  You could insert your own
-  # middleware to handle the JSON parsing and encoding, which is what
-  # SpectraView does.  This example just manually takes care of that down below.
+  # Setup for JSON requests and responses.
   conn.headers = { "Accept" => "application/json",
                    "Content-Type" => "application/json" }
 end
@@ -28,24 +26,49 @@ users.each do | user_entry |
 end
 
 # None of the "/users" responses include the S3 keys.  For that, you have to
-# make a separate "/s3v/keys" request which is setup to return an array
-# of S3 authid/secretkey pairs in case we ever allow more than one pair per
+# make a separate "/ds3/keys" request which is setup to return an array
+# of DS3 authid/secretkey pairs in case we ever allow more than one pair per
 # user.
-response = connection.get("/s3v/keys?user_id=#{spectra_user["id"]}")
+response = connection.get("/ds3/keys?user_id=#{spectra_user["id"]}")
 spectra_user_keys = JSON.parse(response.body)["data"][0]
+abort("Spectra User Keys not found.") if spectra_user.keys.nil?
 
-ENV["DS3_ENDPOINT"] = "sm25-2.eng.sldomain.com"
+ENV["DS3_ENDPOINT"] = "#{ENV['DS3_ENDPOINT']}.eng.sldomain.com"
 ENV["DS3_SECRET_KEY"] = spectra_user_keys["secret_key"]
 ENV["DS3_ACCESS_KEY"] = spectra_user_keys["auth_id"]
 puts "DS3_ENDPOINT #{ENV["DS3_ENDPOINT"]}"
 puts "DS3_SECRET_KEY #{ENV["DS3_SECRET_KEY"]}"
 puts "DS3_ACCESS_KEY #{ENV["DS3_ACCESS_KEY"]}"
 
-puts "docker run -e DS3_ENDPOINT -e DS3_SECRET_KEY -e DS3_ACCESS_KEY -it --dns=10.1.0.9 a8f4f0ebb54c"
-output = `docker run -e DS3_ENDPOINT -e DS3_SECRET_KEY -e DS3_ACCESS_KEY -it --dns=10.1.0.9 a8f4f0ebb54c`
+ENV["GIT_REPO"] = ENV["GIT_REPO"] || "https://github.com/SpectraLogic/ds3_python_sdk.git"
+ENV["GIT_BRANCH"] = ENV["GIT_BRANCH"] || "master"
+puts "GIT_REPO #{ENV["GIT_REPO"] || "default"}"
+puts "GIT_BRANCH #{ENV["GIT_BRANCH"]}"
+
+ENV["DOCKER_REPO"] =  "spectralogic/ds3_python_docker_test:latest"
+puts "DOCKER_REPO #{ENV["DOCKER_REPO"]}"
+ 
+# Close the git repo
+puts `git clone #{ENV["GIT_REPO"]} --branch #{ENV["GIT_BRANCH"]} --single-branch`
+
+# Build latest docker image
+puts "docker build -t #{ENV['DOCKER_REPO']} ./ds3_python_sdk/docker/"
+docker_build_output = `docker build -t #{ENV["DOCKER_REPO"]} ./ds3_python_sdk/docker/`
+docker_build_status = $?
+puts docker_build_output
+puts "docker build status[#{docker_build_status}][#{docker_build_status.exitstatus}]"
+
+# Run the tests in the updated docker container
+puts "docker run -e DS3_ENDPOINT -e DS3_SECRET_KEY -e DS3_ACCESS_KEY -e GIT_REPO -e GIT_BRANCH --dns=10.1.0.9 #{ENV["DOCKER_REPO"]}"
+output = `docker run -e DS3_ENDPOINT -e DS3_SECRET_KEY -e DS3_ACCESS_KEY -e GIT_REPO -e GIT_BRANCH --dns=10.1.0.9 #{ENV["DOCKER_REPO"]}`
+
 docker_status = $?
 
 puts output
 puts "docker status[#{docker_status}][#{docker_status.exitstatus}]"
-exit docker_status.exitstatus
 
+
+# clean up git repo
+puts `rm -rf ds3_python_sdk`
+
+exit docker_status.exitstatus
