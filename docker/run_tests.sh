eef26cc26f466e3311eab3d115f474fbca72ea46
Denver <denverm@spectralogic.com>
2016-07-13 13:00:24 -0600

    Update run_tests.sh

--- docker/run_tests.sh
+++ docker/run_tests.sh
@@ -1,8 +1,6 @@
-
 #!/bin/bash
-echo Using Git Repo: ${GIT_REPO:="https://github.com/SpectraLogic/ds3_c_sdk.git"}
+echo Using Git Repo: ${GIT_REPO:="https://github.com/SpectraLogic/ds3_python_sdk.git"}
 echo Using Git Branch: ${GIT_BRANCH:="master"}
-echo Using Git Sha: ${GIT_SHA:=""}
 
 echo DS3_ENDPOINT ${DS3_ENDPOINT}
 echo DS3_SECRET_KEY ${DS3_SECRET_KEY}
@@ -19,19 +17,7 @@ else
   git clone ${GIT_REPO}
 fi
 
-if [${GIT_SHA} != "" ]; then
-  echo setting to SHA1 ${GIT_SHA}
-  git reset --hard ${GIT_SHA}
-fi
-
-cd ds3_c_sdk
-autoreconf --install
-./configure
-make install
-ldconfig
-
-cd /opt
-git clone https://github.com/SpectraLogic/ds3_python_sdk.git
+echo "cd ds3_python_sdk"
 cd ds3_python_sdk
 python setup.py install
 
