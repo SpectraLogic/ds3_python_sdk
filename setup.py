0d6c416ebcbb7e2d124edc3e2c9027707ab19bcb
Ryan Moore <ryan.moore@greyrocksoft.com>
2017-02-27 16:12:26 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from distutils.core import setup
 
 setup(name='DS3 SDK',
-    version='3.2.1',
+    version='3.4.1',
     description='Python SDK and CLI for Spectra S3',
     author_email='developer@spectralogic.com',
     packages=['ds3'])
